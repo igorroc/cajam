d5ec4d55589c2be1645e78ab31e101ad898fde35
Igor <igor_roc@hotmail.com.br>
2023-02-01 22:10:44 -0300

    fix: Remove comments

--- js/loader.js
+++ js/loader.js
@@ -1,8 +1,6 @@
 window.addEventListener("load", async () => {
 	await setTimeout(() => {
-		console.log("loaded")
 		const loader = document.querySelector("#loader")
-		console.log(loader)
 		loader.classList.add("loaded")
 	}, 1000)
 })
