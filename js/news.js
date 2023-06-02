07f1f33fdaa7223395728292bad94f319e8b7d1c
belorenata <135391623+belorenata@users.noreply.github.com>
2023-06-02 20:24:17 -0300

    Update news.js

--- js/news.js
+++ js/news.js
@@ -2,19 +2,11 @@ const newsWrapper = document.querySelector("#news-wrapper")
 
 const news = [
 	{
-		title: "Segundo Seminário IBADPP",
-		image: "https://dl1.instavideosave.com/?url=https%3A%2F%2Fscontent-cdg4-1.cdninstagram.com%2Fv%2Ft51.36329-15%2F337336366_750535916457163_4693296022168756848_n.jpg%3Fstp%3Ddst-jpg_s640x640%26_nc_cat%3D102%26ccb%3D1-7%26_nc_sid%3D8ae9d6%26_nc_ohc%3DODKlN0o8X3MAX_3ypyx%26_nc_ht%3Dscontent-cdg4-1.cdninstagram.com%26oh%3D00_AfDyRkeGVk93OG4W2Lt-dqr28cWza_MXHxEsefV3MSjWkg%26oe%3D644DD9C4&type=mp4&dlheader=image/jpeg&title=62600680960",
-		url: "https://instagram.com/ibadpp?igshid=YmMyMTA2M2Y=",
-		date: "25/05/2023",
-		content:
-			"O Instituto Baiano de Direito Processual Penal (IBADPP) realizará o segundo seminário regional na UESC nos dias 25 e 26 de maio, realizando a sua inscrição para garantir a sua vaga.",
-	},
-	{
 		title: "Eleições para o Centro Acadêmico",
 		image: "https://drive.google.com/u/0/uc?id=1rEn5yMmjil-JMyWPIq9H0eNEyA21vole&export=download",
 		date: "05/05/2023",
 		content:
-			"Conheça os integrantes e propostas da chapa 02, as eleições irão acontecer no dia 05 de Maio, leve a sua identidade com foto e vote certo na urna.",
+			"Conheça os integrantes e propostas da chapa 02, ainda não há uma data para a eleição, leve a sua identidade com foto e vote certo na urna.",
 	},
 	{
 		title: "dicas_salobrinho",
