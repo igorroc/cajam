15a8133958e73df304683394d2c877f4aa4d0842
Igor Rocha <igor_roc@hotmail.com.br>
2023-06-02 20:02:05 -0300

    feat: Prettier

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,9 +1,9 @@
 document.addEventListener("mousemove", function (ev) {
-    let e = ev || window.event;
+	let e = ev || window.event
 	let tooltips = document.querySelectorAll(".tooltipMouse")
 	let mouseX = e.clientX
 	let mouseY = e.clientY
-    let scroll = window.scrollY
+	let scroll = window.scrollY
 	tooltips.forEach((tooltip) => {
 		tooltip.style.setProperty("--mouseX", `${mouseX}px`)
 		tooltip.style.setProperty("--mouseY", `${mouseY + scroll}px`)
